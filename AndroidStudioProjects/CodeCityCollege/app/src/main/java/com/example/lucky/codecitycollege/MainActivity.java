434264abdfcaf0afbc9744cb7264581e84db8e0f
Lucky <Lumpsey@gmail.com>
2017-08-25 10:52:08 +0100

    corrected commit

---
+++ AndroidStudioProjects/CodeCityCollege/app/src/main/java/com/example/lucky/codecitycollege/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lucky.codecitycollege;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //test commit
+    }
+    public void latest(){
+        int i =1;
+        //recent change
+    }
+}
